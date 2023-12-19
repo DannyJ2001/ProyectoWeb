eeb30fe9adbe2ff3c67c57a3ce2a73ee433fee7d
DannyJ2001 <108770227+DannyJ2001@users.noreply.github.com>
2023-12-19 10:47:52 -0500

    first commit

---
+++ build/generated-sources/ap-source-output/modelo/Empleado_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Departamento;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-12-18T11:34:36")
+@StaticMetamodel(Empleado.class)
+public class Empleado_ { 
+
+    public static volatile SingularAttribute<Empleado, Departamento> idDep;
+    public static volatile SingularAttribute<Empleado, String> nombre;
+    public static volatile SingularAttribute<Empleado, Integer> idempleado;
+
+}
